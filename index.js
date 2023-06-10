42bf83f115c0694e460256af942a81730e8789e5
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-06-09 23:39:32 -0600

    classextension

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+      this._sides = sides;
+    }
+  
+    get countSides() {
+      return this._sides.length;
+    }
+  
+    get perimeter() {
+      return this._sides.reduce((acc, side) => acc + side, 0);
+    }
+  }
+  class Triangle extends Polygon {
+    constructor(sides) {
+      super(sides);
+    }
+  
+    get isValid() {
+      const [a, b, c] = this._sides;
+      return a + b > c && a + c > b && b + c > a;
+    }
+  }
+  class Square extends Polygon {
+    constructor(sides) {
+      super(sides);
+    }
+  
+    get isValid() {
+      const [side1, side2, side3, side4] = this._sides;
+      return side1 === side2 && side1 === side3 && side1 === side4;
+    }
+  
+    get area() {
+      const [side] = this._sides;
+      return side * side;
+    }
+  }
+      
